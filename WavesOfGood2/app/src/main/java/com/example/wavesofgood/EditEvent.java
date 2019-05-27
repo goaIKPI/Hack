00f77867ff3a1ef01f3d46a84cc3ab1f5bddbe09
Bioman1464 <danilgap1464@gmail.com>
2019-05-27 10:46:17 +0500

    gg 2.0

---
+++ WavesOfGood2/app/src/main/java/com/example/wavesofgood/EditEvent.java
@@ -0,0 +1,13 @@
+package com.example.hack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editing_event_activity);
+    }
+}
--- WavesOfGood2/app/src/main/java/com/example/wavesofgood/HelpCompany.java
+++ WavesOfGood2/app/src/main/java/com/example/wavesofgood/HelpCompany.java
@@ -5,8 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class HelpCompany extends AppCompatActivity {
 
@@ -26,8 +29,17 @@ public class HelpCompany extends AppCompatActivity {
         final Button btn2 = findViewById(R.id.endBtn1);
         final Button btn3 = findViewById(R.id.startBtn);
         final Button btn4 = findViewById(R.id.startBtn1);
-        final TextView tv1 = findViewById(R.id.activeTxtView);
-        final TextView tv2 = findViewById(R.id.activeTxtView1);
+
+        TextView tv1 = findViewById(R.id.helpName1);
+        TextView tv2 = findViewById(R.id.helpTypeTxtView1);
+        TextView tv3 = findViewById(R.id.peopleAmmountTxtView1);
+        Button btnEdit = findViewById(R.id.editItemHelpBtn1);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
